bf91bc6a9074636a1c012b377c89547a5daaa006
Vinícius Rodrigues <vinird1993@gmail.com>
2018-07-05 22:03:57 -0300

    Create App Notas Diarias

---
+++ Notas Diarias/Notas Diarias/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notas Diarias
+//
+//  Created by Vinícius Rodrigues Duarte on 05/07/18.
+//  Copyright © 2018 Vinícius Rodrigues Duarte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
