f03b4226b78ea5119dec8e25104e578f807e295e
Vinicius Rodrigues <viniciusrodrigues@ioasys.com.br>
2019-03-16 11:08:54 -0300

    .

--- CampoesDasCopas/CampoesDasCopas/Controllers/WorldCupViewController.swift
+++ CampoesDasCopas/CampoesDasCopas/Controllers/WorldCupViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class WorldCupViewController: UIViewController {
 
+    @IBOutlet weak var ivWinner: UIImageView!
+    @IBOutlet weak var ivVice: UIImageView!
+    @IBOutlet weak var lbScore: UILabel!
+    @IBOutlet weak var lbWinner: UILabel!
+    @IBOutlet weak var lbVice: UILabel!
+    
+    
     var worldCup: WorldCup!;
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "World Cup \(worldCup.year)"
+        ivWinner.image =  UIImage(named: "\(worldCup.winner).png")
+        ivVice.image =  UIImage(named: "\(worldCup.vice).png")
+        lbWinner.text =  worldCup.winner
+        lbVice.text =  worldCup.winner
+        lbScore.text = "\(worldCup.winnerScore) x \(worldCup.viceScore)"
+        
     }
     
 }
