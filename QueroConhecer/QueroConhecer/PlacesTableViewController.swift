b978d4eef6d9631ef65005710f9ea014db90f749
Vinícius Rodrigues <vinird1993@gmail.com>
2019-03-18 22:39:49 -0300

    ajustes

--- QueroConhecer/QueroConhecer/PlacesTableViewController.swift
+++ QueroConhecer/QueroConhecer/PlacesTableViewController.swift
@@ -12,16 +12,25 @@ class PlacesTableViewController: UITableViewController {
     
     var places: [ Place] = []
     var userDefaults = UserDefaults.standard
+    var lbNoPlaces: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         loadPlaces()
+        
+        lbNoPlaces = UILabel()
+        lbNoPlaces.text = "Cadastre os locais que desenha conhecer\n clicando no botao + acima"
+        lbNoPlaces.textAlignment = .center
+        lbNoPlaces.numberOfLines = 0
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier != "mapSegue"{
             let viewController = segue.destination as! PlacesFinderViewController
             viewController.delegate = self
+        }else{
+            let vc = segue.destination as! MapViewController
         }
     }
     
@@ -44,6 +53,11 @@ class PlacesTableViewController: UITableViewController {
         }
     }
 
+    @objc func showAll(){
+        
+    }
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -52,7 +66,14 @@ class PlacesTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
+        if places.count>0{
+            let btShowAll = UIBarButtonItem(title: "Mostrar todos no mapa", style: .plain, target: self, action: #selector(showAll))
+            navigationItem.leftBarButtonItem = btShowAll
+            tableView.backgroundView = nil
+        }else{
+            navigationItem.leftBarButtonItem = nil
+        }
         return places.count
     }
 
@@ -64,51 +85,21 @@ class PlacesTableViewController: UITableViewController {
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let place = places[indexPath.row]
+        performSegue(withIdentifier: "mapSegue", sender: place)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
+    
+    //Swipte para delete
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            places.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            savePlaces();
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
 
